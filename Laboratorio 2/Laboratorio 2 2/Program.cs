f000e9737f1c87d58e40af4650dcded4304ec1cf
Jonathan Lasso <jonathan.lasso30@gmail.com>
2025-09-15 19:12:31 -0500

    Lab 2 1 y Lab 2 2 terminado.

---
+++ Laboratorio 2/Laboratorio 2 2/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        MyClass.valor = 1;
+        Console.WriteLine(MyClass.valor);
+    }
+
+    public class MyClass
+    {
+        public static int valor;
+    }
+}
---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(String[] args)
+    {
+        Client client = new Client();
+        client.FirstName = "Jonathan";
+        client.LastName = "Lasso";
+        client.Age = 19;
+        client.Id = 1;
+
+        Console.WriteLine(client.GetFullName());
+    }
+
+    public class Client
+    {
+        public int Id
+        {
+            get; set;
+        }
+        public string FirstName
+        {
+            get; set;
+        }
+
+        public string LastName
+        {
+            get; set;
+        }
+
+        public ushort Age
+        {
+            get; set;
+        }
+
+        public string GetFullName ()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
