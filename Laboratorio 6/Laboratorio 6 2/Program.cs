8a7330dca1d505c289a9044156e1f362ae32bc20
Jonathan Lasso <jonathan.lasso30@gmail.com>
2025-09-22 20:03:30 -0500

    Todo el laboratorio 6 hasta el 6-4 terminado.

---
+++ Laboratorio 6/Laboratorio 6 2/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+        Console.WriteLine("Digite el numero deseado");
+
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+        }catch (FormatException e)
+        {
+            Console.WriteLine("No se ha introducido un digito valido");
+            num = -1;
+        }catch (OverflowException a)
+        {
+            Console.WriteLine("El numero introducido es muy grande");
+            num = -1;
+        }
+
+        Console.WriteLine(num);
+    }
+}
---
+++ Laboratorio 6/Laboratorio 6 4/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void cheackAge (int age)
+    {
+        if(age < 10)
+        {
+            throw new ArithmeticException("Acceso negado - No cumple con el criterio de edad");
+        }
+        else
+        {
+            Console.WriteLine("Acceso Consedido");
+        }
+    }
+    static void Main(string[] args)
+    {
+        cheackAge(15);
+        cheackAge(5);
+    }
+}
