a124a12ab88cad76b623dcd24f9d36b3bc77f2fb
Jonathan Lasso <jonathan.lasso30@gmail.com>
2025-10-14 10:38:15 -0500

    Todo el laboratorio 13 terminado.

---
+++ Laboratorio 13/Laboratorio 13/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Laboratorio_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectarDesconectarSql = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnConectarDesconectarSql
+            // 
+            this.btnConectarDesconectarSql.Location = new System.Drawing.Point(306, 59);
+            this.btnConectarDesconectarSql.Name = "btnConectarDesconectarSql";
+            this.btnConectarDesconectarSql.Size = new System.Drawing.Size(163, 59);
+            this.btnConectarDesconectarSql.TabIndex = 0;
+            this.btnConectarDesconectarSql.Text = "Conectar y desconectar SQL Server";
+            this.btnConectarDesconectarSql.UseVisualStyleBackColor = true;
+            this.btnConectarDesconectarSql.Click += new System.EventHandler(this.btnConectarDesconectarSql_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(240, 161);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(271, 132);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnConectarDesconectarSql);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectarDesconectarSql;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Laboratorio 13/Laboratorio 13/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Laboratorio_13
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = 
+            @"Server=localhost;
+            Database=Northwind;
+            Trusted_Connection=True;
+            TrustServerCertificate=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectarDesconectarSql_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conexion = new SqlConnection(connectionString);
+
+                conexion.Open();
+
+                MessageBox.Show("Se abrió la conexión con el servidor SQL Server y se seleccionó la base de datos");
+
+                string query = "SELECT ProductName FROM Products";
+
+                SqlCommand cmd = new SqlCommand(query, conexion);
+                
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while(rdr.Read())
+                {
+                    string producto = $"{rdr["ProductName"]}";
+                    listBox1.Items.Add(producto);
+                }
+                
+                conexion.Close();
+                MessageBox.Show("Se cerró la conexión.");
+
+            }catch (Exception z)
+            {
+                MessageBox.Show("No se pudo conectar a la base de datos.");
+            }
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
