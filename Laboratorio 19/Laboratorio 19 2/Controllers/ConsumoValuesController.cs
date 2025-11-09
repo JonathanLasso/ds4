63b0c38eddf932d75b9c525daaeff74e15de3050
Jonathan Lasso <jonathan.lasso30@gmail.com>
2025-11-09 15:09:35 -0500

    Laboratorio 19 terminado.

---
+++ Laboratorio 19/Laboratorio 19 2/Controllers/ConsumoValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio_19_2.Controllers
+{
+    public class ConsumoValuesController : Controller
+    {
+        // GET: ConsumoValues
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetItems()
+        {
+            string url = "https://localhost:44308/api/Values/Get";
+            string responseBody = "";
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                request.Accept = "application/json";
+
+                using (WebResponse response = request.GetResponse())
+                using (Stream strReader = response.GetResponseStream())
+                {
+                    if (strReader == null)
+                        return Content("No hay respuesta del servidor.");
+                    using (StreamReader objReader = new StreamReader(strReader))
+                    {
+                        responseBody = objReader.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                responseBody = "Error al consumir el servicio: " + ex.Message;
+            }
+
+            ViewBag.Index = responseBody;
+            return View("Index");
+        }
+    }
+}
---
+++ Laboratorio 19/Laboratorio 19 3/Controllers/ConsumoValue2Controller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio_19_3.Controllers
+{
+    public class ConsumoValue2Controller : Controller
+    {
+        // GET: ConsumoValue2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetItem()
+        {
+            string url = "https://localhost:44308/api/Values/Get/2";
+            string responseBody = "";
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                request.Accept = "application/json";
+
+                using (WebResponse response = request.GetResponse())
+                using (Stream strReader = response.GetResponseStream())
+                {
+                    if (strReader == null)
+                        return Content("No hay respuesta del servidor.");
+                    using(StreamReader objReader = new StreamReader(strReader))
+                    {
+                        responseBody = objReader.ReadToEnd();
+                    }
+                }
+            }catch (WebException ex)
+            {
+                responseBody = "Error al consumir el servicio: " + ex.Message;
+            }
+
+            ViewBag.Index = responseBody;
+            return View("Index");
+        }
+    }
+}
